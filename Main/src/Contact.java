64aceee606b9655b81f10aa7c667b363e20dd5b0
simplystic <14334053+simplystic@users.noreply.github.com>
2019-04-28 21:18:44 +0200

    Add files via upload

---
+++ Main/src/Contact.java
@@ -0,0 +1,11 @@
+public class Contact {
+    public Integer ID;
+    public String firstName;
+    public String lastName;
+    public String phone;
+    public String email;
+    public Integer sexID;
+    public Integer addressID;
+
+
+}
---
+++ Main/src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public Contact contact;
+    public static void main(String[] args) {
+        Contact contact = new Contact();
+        System.out.println("Croz!");
+        System.out.println(contact);
+
+    }
+}
